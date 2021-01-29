08d8e205694192f685a297673d2796a30c13ecd6
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-01-29 12:31:42 +0700

    Выполнил второе задание

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ let addExpenses = 'Кино, кафе, спортзал';
 let deposit = true;
 let mission = 10000000;
 let period = 6;
+let budgetDay = 'доход за месяц/30'
 
-
-
-
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof addExpenses);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработь ' + mission + ' рублей');
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
